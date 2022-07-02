15954bfe310623885272919f23a4bdddcaf997a3
BAKAJ77 <44305796+BAKAJ77@users.noreply.github.com>
2022-07-02 02:44:26 +0100

    Added response action to the EXIT button being pressed
    Also adjusted the position of the buttons and the title texture.

--- src/states/main_menu.cpp
+++ src/states/main_menu.cpp
@@ -10,27 +10,38 @@ void MainMenu::Init()
     // Initialize the user interface
     this->menuGUI = UserInterface(this->GetAppWindow(), 8.0f, 0.0f);
 
-    this->menuGUI.AddStandaloneButton(new MenuButton({ 250, 960 }, { 450, 200 }, { 465, 215 }, "PLAY"));
-    this->menuGUI.AddStandaloneButton(new MenuButton({ 720, 960 }, { 450, 200 }, { 465, 215 }, "SETTINGS"));
-    this->menuGUI.AddStandaloneButton(new MenuButton({ 1190, 960 }, { 450, 200 }, { 465, 215 }, "HELP"));
-    this->menuGUI.AddStandaloneButton(new MenuButton({ 1660, 960 }, { 450, 200 }, { 465, 215 }, "EXIT"));
+    this->menuGUI.AddStandaloneButton(new MenuButton({ 250, 750 }, { 450, 200 }, { 465, 215 }, "PLAY"));
+    this->menuGUI.AddStandaloneButton(new MenuButton({ 720, 750 }, { 450, 200 }, { 465, 215 }, "SETTINGS"));
+    this->menuGUI.AddStandaloneButton(new MenuButton({ 1190, 750 }, { 450, 200 }, { 465, 215 }, "HELP"));
+    this->menuGUI.AddStandaloneButton(new MenuButton({ 1660, 750 }, { 450, 200 }, { 465, 215 }, "EXIT"));
 }
 
-void MainMenu::Destroy()
-{
-}
+void MainMenu::Destroy() {}
 
 void MainMenu::Update(const float& deltaTime)
 {
-    if (!this->paused)
+    if (!this->paused) // Only update when the app state is not paused
+    {
+        // Update the menu user interface
         this->menuGUI.Update(deltaTime);
+
+        // Check and respond if any of the standalone buttons are pressed
+        const std::vector<ButtonBase*>& buttons = this->menuGUI.GetStandaloneButtons();
+
+        for (size_t index = 0; index < buttons.size(); index++)
+        {
+            const MenuButton* menuButton = (MenuButton*)buttons[index];
+            if (menuButton->GetText() == "EXIT" && menuButton->WasClicked())
+                this->PopState();
+        }
+    }
 }
 
 void MainMenu::Render() const
 {
     Renderer::GetInstance().RenderSquare({ 960, 540 }, { 1920, 1080 }, TextureLoader::GetInstance().GetTexture("Background 2"),
         { 255, 255, 255, this->bkgOpacity });
-    Renderer::GetInstance().RenderSquare({ 136, 52 }, { 232, 63 }, TextureLoader::GetInstance().GetTexture("Title Logo Small"),
+    Renderer::GetInstance().RenderSquare({ 960, 400 }, { 1280, 345 }, TextureLoader::GetInstance().GetTexture("Title Logo Large"),
         { 255, 255, 255, this->menuGUI.GetOpacity() });
 
     this->menuGUI.Render();
